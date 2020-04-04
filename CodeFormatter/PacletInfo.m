13f49130dbb113b4195e416529a4655d84b36c80
Brenton Bostick <brenton@wolfram.com>
2020-04-04 10:28:03 -0500

    Bump to 1.1

--- CodeFormatter/PacletInfo.m
+++ CodeFormatter/PacletInfo.m
@@ -1,7 +1,7 @@
 
 Paclet[
   Name -> "CodeFormatter",
-  Version -> "1.0",
+  Version -> "1.1",
   WolframVersion -> "11.0+",
   Description -> "Provide functionality for formatting Wolfram Language code.",
   Creator -> "Brenton Bostick <brenton@wolfram.com>",
